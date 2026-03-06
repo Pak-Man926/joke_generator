e72381dcd0699975d9d01e9f5ecec8d54f5919f2
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-06 17:58:52 +0300

    Defining the UI

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import "package:flutter/material.dart";
+
+class HomeView extends StatelessWidget
+{
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar:AppBar(
+        title: const Text("Random Joke Generator"),
+        centerTitle: true,
+      ),
+      body: SizedBox.expand(
+        child: Stack(
+          alignment: Alignment.center,
+          children:[
+            const SelectableText(
+              "What kind of bagel can fly? \n\n" "A plain bagel!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize:24)
+            ),
+            Positioned(
+              bottom: 20,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Get another joke"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
