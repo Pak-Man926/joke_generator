a36441911fc5572e4e44154dbd08f280cc968f02
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-09 14:18:44 +0300

    Reconfiguring code flow

--- lib/home.dart
+++ lib/home.dart
@@ -12,42 +12,44 @@ class HomeView extends StatelessWidget {
         title: const Text("Random Joke Generator"),
         centerTitle: true,
       ),
-      body: SizedBox.expand(
-        child: Consumer(
-          builder: (context, ref, child) {
-            final randomJoke = ref.watch(randomJokeProvider);
-            return Stack(
-              alignment: Alignment.center,
-              children: [
-                switch (randomJoke) {
-                  AsyncValue(:final value?) => SelectableText(
-                    "${value.setup} \n\n ${value.punchline}",
-                    textAlign: .center,
-                    style: TextStyle(fontSize: 24),
+      body: SafeArea(
+        bottom: true,
+        child: SizedBox.expand(
+          child: Consumer(
+            builder: (context, ref, child) {
+              final randomJoke = ref.watch(randomJokeProvider);
+              return Stack(
+                alignment: Alignment.center,
+                children: [
+                  if (randomJoke.isRefreshing)
+                    const Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      child: LinearProgressIndicator(),
+                    ),
+                  switch (randomJoke) {
+                    AsyncValue(:final value?) => SelectableText(
+                      "${value.setup} \n\n ${value.punchline}",
+                      textAlign: .center,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    AsyncValue(error: != null) => const Text(
+                      "Error fetching joke",
+                    ),
+                    AsyncValue() => CircularProgressIndicator(),
+                  },
+                  Positioned(
+                    bottom: 20,
+                    child: ElevatedButton(
+                      onPressed: () => ref.invalidate(randomJokeProvider),
+                      child: const Text("Get another joke"),
+                    ),
                   ),
-                  AsyncValue(error: != null) => const Text(
-                    "Error fetching joke",
-                  ),
-
-                  AsyncValue() => CircularProgressIndicator(),
-                },
-                if (randomJoke.isRefreshing)
-                  const Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: LinearProgressIndicator(),
-                  ),
-                Positioned(
-                  bottom: 20,
-                  child: ElevatedButton(
-                    onPressed: () => ref.invalidate(randomJokeProvider),
-                    child: const Text("Get another joke"),
-                  ),
-                ),
-              ],
-            );
-          },
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
