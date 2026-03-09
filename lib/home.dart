78b0904e75ac179a2b2079fab5f14649f50ea97a
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-09 13:53:24 +0300

    State initialization in the homescreen, and loading indicator positioning.

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,55 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:joke_generator/joke.dart";
 
-class HomeView extends StatelessWidget
-{
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: const Text("Random Joke Generator"),
         centerTitle: true,
       ),
       body: SizedBox.expand(
-        child: Stack(
-          alignment: Alignment.center,
-          children:[
-            const SelectableText(
-              "What kind of bagel can fly? \n\n" "A plain bagel!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize:24)
-            ),
-            Positioned(
-              bottom: 20,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Get another joke"),
-              ),
-            ),
-          ],
+        child: Consumer(
+          builder: (context, ref, child) {
+            final randomJoke = ref.watch(randomJokeProvider);
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                switch (randomJoke) {
+                  AsyncValue(:final value?) => SelectableText(
+                    "${value.setup} \n\n ${value.punchline}",
+                    textAlign: .center,
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  AsyncValue(error: != null) => const Text(
+                    "Error fetching joke",
+                  ),
+
+                  AsyncValue() => CircularProgressIndicator(),
+                },
+                if (randomJoke.isRefreshing)
+                  const Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: LinearProgressIndicator(),
+                  ),
+                Positioned(
+                  bottom: 20,
+                  child: ElevatedButton(
+                    onPressed: () => ref.invalidate(randomJokeProvider),
+                    child: const Text("Get another joke"),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
   }
-
-
 }
